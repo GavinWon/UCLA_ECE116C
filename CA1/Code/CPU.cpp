db25c5c0bfacfc0e1fcc44976d1468126bfad0a8
Gavin Wong <gavinmwong@gmail.com>
2023-10-28 16:03:20 -0700

    Initial Commit - Finished CA1

---
+++ CA1/Code/CPU.cpp
@@ -0,0 +1,239 @@
+#include "CPU.h"
+
+instruction::instruction(bitset<32> fetch)
+{
+	//cout << fetch << endl;
+	instr = fetch;
+	//cout << instr << endl;
+}
+
+CPU::CPU()
+{
+	for(int i = 0; i < 32; i++) {
+   		registerFile[i] = 0;
+	}
+	PC = 0; //set PC to 0
+	for (int i = 0; i < 4096; i++) //copy instrMEM
+	{
+		dmemory[i] = (0);
+	}
+	CPU::Reset();
+
+}
+
+void CPU::Reset() {
+	Jump = 0;
+	Branch = 0;
+	MemRead = 0;
+	MemToReg = 0;
+	ALUOp = 0;
+	MemWrite = 0;
+	ALUSrc = 0;
+	RegWrite = 0;
+
+	// Decode Stage
+	operation = Op::ZE;
+	opcode = 0;
+	func3 = 0;
+	func7 = 0;
+	rs1 = 0;
+	rs2 = 0;
+	rd = 0;
+	immediate = 0;
+
+	// Execute Stage
+	readData1 = 0;
+	readData2 = 0;
+	LessThan = 0;
+
+	// Memory Stage
+	aluResult = 0;
+	dataMemoryResult = 0;
+}
+
+bitset<32> CPU::Fetch(bitset<8> *instmem) {
+	//cout << "Fetching" << endl;
+	bitset<32> instr = ((((instmem[PC + 3].to_ulong()) << 24)) + ((instmem[PC + 2].to_ulong()) << 16) + ((instmem[PC + 1].to_ulong()) << 8) + (instmem[PC + 0].to_ulong()));  //get 32 bit instruction
+	//PC += 4;//increment PC
+	return instr;
+}
+
+
+bool CPU::Decode(instruction* curr)
+{
+	//cout<<curr->instr<<endl;
+	//cout << "Decoding" << endl;
+	opcode = curr->instr.to_ulong() & 0x7f; //0b1111111
+	func3 = (curr->instr.to_ulong() >> 12) & 0x7;  //0b111
+	func7 = (curr->instr.to_ulong() >> 25) & 0x7f; //0b1111111
+	rs1 = (curr->instr.to_ulong() >> 15) & 0x1f; //0b11111
+	rs2 = (curr->instr.to_ulong() >> 20) & 0x1f; //0b11111
+	rd = (curr->instr.to_ulong() >> 7) & 0x1f; //0b11111
+
+	if (opcode == RTYPE) {
+		if (func3 == 0x0) {
+			if (func7 == 0x0) {ALUOp = ALUadd ; operation = Op::ADD;}
+			else if (func7 == 0x20) {ALUOp = ALUsub; operation = Op::SUB;}
+			else {operation = Op::ERROR;}
+		} 
+		else if (func3 == 0x4) {ALUOp = ALUxor; operation = Op::XOR;}
+		else if (func3 == 0x5) {ALUOp = ALUsra; operation = Op::SRA;}
+		RegWrite = 1;
+		ALUSrc = 1;
+	} else if (opcode == ITYPE) {
+		if (func3 == 0x0) {ALUOp = ALUadd; operation = Op::ADDI;}
+		else if (func3 == 0x7) {ALUOp = ALUand; operation = Op::ANDI;}
+		immediate = (curr->instr.to_ulong() >> 20);
+		RegWrite = 1;
+	} else if (opcode == LWORD && func3 == 0x2) {
+		ALUOp = ALUadd; 
+		operation = Op::LW;
+		immediate = (curr->instr.to_ulong() >> 20);
+		MemRead = 1;
+		RegWrite = 1;
+		MemToReg = 1;
+	} else if (opcode == SWORD && func3 == 0x2) {
+		ALUOp = ALUadd;
+		operation = Op::SW;
+		int32_t imm11_5 = curr->instr.to_ulong() & 0xfe000000;
+		int32_t imm4_0 = (curr->instr.to_ulong() & 0xf80) << 13;
+		immediate = (imm11_5 + imm4_0) >> 20; //Redefined for SW
+		MemWrite = 1;
+	} else if (opcode == JTYPE && func3 == 0x0) {
+		//cout << "Jump Instruction" << endl;
+		ALUOp = ALUadd;
+		operation = Op::JALR;
+		immediate = (curr->instr.to_ulong() & 0x80000000) ? (curr->instr.to_ulong() >> 20) | 0xFFFFF800 : (curr->instr.to_ulong() >> 20);
+		Jump = 1;
+		RegWrite = 1;
+	} else if (opcode == BTYPE && func3 == 0x4) {
+		//cout << "Branch Instruction" << endl;
+		ALUOp = ALUsub;
+		operation = Op::BLT;
+		auto imm12 = (curr -> instr.to_ulong() & 0x80000000) ? 0xFFFFF800 : 0x0;// Bit 31 (sign extend for negative values)
+		auto imm10_5 = (curr -> instr.to_ulong() & 0x7E000000) >> 20; //bits 30-25
+		auto imm4_1 = (curr -> instr.to_ulong() & 0xF00) >> 7; // bits 11-8
+		auto imm11 = (curr->instr.to_ulong() & 0x80) << 4; //bit 7
+		immediate = imm12 | imm11 | imm10_5 | imm4_1;
+		Branch = 1;
+		ALUSrc = 1;
+	}
+	else if (opcode == ZERO) {
+		operation = Op::ZE;
+		return false;
+	}
+	//cout << opcode << endl;
+	// Still need BLT and JALR
+	readData1 = registerFile[rs1];
+	readData2 = registerFile[rs2];
+	return true;
+}
+
+bool CPU::Execute() {
+	//cout << "Executing" << endl;
+	aluResult = 0;
+	int32_t input1, input2;
+	input1 = readData1;
+	input2 = (ALUSrc == 1) ? registerFile[rs2] : immediate;
+	switch(ALUOp) {
+		case ALUadd:
+			aluResult = input1 + input2;
+			break;
+		case ALUsub:
+			aluResult = input1 - input2;
+			break;
+		case ALUand:
+			aluResult = input1 & input2;
+			break;
+		case ALUxor:
+			aluResult = input1 ^ input2;
+			break;
+		case ALUsra:
+			aluResult = input1 >> input2;
+			break;
+		default:
+			return false;
+	}
+	if (aluResult < 0) {
+		LessThan = 1;
+	}
+	
+	return true;
+}
+
+bool CPU::Memory() {
+	//cout << "Memory" << endl;
+	if (MemWrite == 1) { //Store Word
+		int32_t sByte1, sByte2, sByte3, sByte4;
+		sByte1 = (readData2 >> 24) & 0xff;
+		sByte2 = (readData2 >> 16) & 0xff;
+		sByte3 = (readData2 >> 8) & 0xff;
+		sByte4 = (readData2) & 0xff;
+
+		//cout << "Store Bytes " << sByte1 << sByte2 << sByte3 << sByte4 << endl;
+		
+		// Storing the bytes in dmemory
+		dmemory[aluResult] = sByte4; //LSB
+		dmemory[aluResult + 1] = sByte3;
+		dmemory[aluResult + 2] = sByte2;
+		dmemory[aluResult + 3] = sByte1;
+
+		//cout << "Storing: " << readData2 << " at location " << aluResult << endl;
+	}
+
+	if (MemRead == 1) { //Load Word
+		int32_t lByte1, lByte2, lByte3, lByte4;
+		lByte1 = dmemory[aluResult];
+		lByte2 = dmemory[aluResult + 1];
+		lByte3 = dmemory[aluResult + 2];
+		lByte4 = dmemory[aluResult + 3]; 
+
+		dataMemoryResult = (lByte4 << 24) + (lByte3 << 16) + (lByte2 << 8) + lByte1;
+
+		//cout << "Loading result at: " << aluResult << ", which has value of " << dataMemoryResult << " into register " << rd << endl;
+	}
+	return true;
+
+}
+
+bool CPU::WriteBack() {
+	//cout << "WriteBack" << endl;
+	if (RegWrite == 1 and rd != ZERO) {
+		if (MemToReg == 1) {
+			//cout << "should never run";
+			registerFile[rd] = dataMemoryResult;
+		} else if (Jump == 1) {
+			registerFile[rd] = PC + 4;
+		}
+		else {
+			//cout << "Writing result: " << aluResult << " to register " << rd << endl;
+			registerFile[rd] = aluResult;
+		}
+	}
+	return true;
+}
+
+bool CPU::NextInstruction() {
+	//cout << "Next Instruction" << endl;
+	if (Jump == 1) {
+		PC = aluResult;
+	} else if (Branch == 1 && LessThan == 1) {
+		PC = PC + immediate;
+	} else {
+		PC = PC + 4;
+	}
+	CPU::Reset();
+	return true;
+}
+
+std::pair<int32_t, int32_t> CPU::GetReturnRegisters() {
+    return {registerFile[10], registerFile[11]};
+}
+
+
+unsigned long CPU::readPC()
+{
+	return PC;
+}
+
+// Add other functions here ... 
---
+++ CA1/Code/CPU.h
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+#define ZERO 0x0
+#define RTYPE 0x33
+#define ITYPE 0x13
+#define LWORD 0x3
+#define SWORD 0x23
+#define BTYPE 0x63
+#define JTYPE 0x67
+
+#define ALUadd 0x0
+#define ALUsub 0x1
+#define ALUand 0x2
+#define ALUxor 0x3
+#define ALUsra 0x4
+
+class instruction {
+public:
+	bitset<32> instr; // instruction
+	instruction(bitset<32> fetch); // constructor
+};
+
+enum class Op {
+	ZE, ERROR, ADD, SUB, ADDI, XOR, ANDI, SRA, LW, SW, BLT, JALR,
+};
+
+class CPU {
+private:
+	int dmemory[4096]; // data memory byte addressable in little endian fashion;
+	unsigned long PC; 
+	int32_t registerFile[32];  // Registers x0-x31
+	uint32_t Jump;
+	uint32_t Branch;
+	uint32_t MemRead;
+	uint32_t MemToReg;
+	uint32_t ALUOp;
+	uint32_t MemWrite;
+	uint32_t ALUSrc;
+	uint32_t RegWrite;
+
+	// Decode Stage
+	Op operation;
+	uint32_t opcode;
+	uint32_t func3;
+	uint32_t func7;
+	uint32_t rs1;
+	uint32_t rs2;
+	uint32_t rd;
+	uint32_t immediate;
+
+	// Execute Stage
+	int32_t readData1;
+	int32_t readData2;
+	uint32_t LessThan;
+
+	// Memory Stage
+	int32_t dataMemoryResult;
+	int32_t aluResult; // Added missing semicolon here
+
+	// Statistics
+	unsigned short killCounter;
+	unsigned int clockCount;
+	unsigned int rTypeCount;
+	unsigned int iTypeCount;
+	unsigned int swCount;
+	unsigned int lwCount;
+	unsigned int jCount;
+	unsigned int bCount;
+	unsigned int totalInsCount; // Corrected to 'unsigned int'
+	double ipc;
+
+public:
+	CPU();
+	unsigned long readPC();
+	bitset<32> Fetch(bitset<8> *instmem);
+	void Reset();
+	bool Decode(instruction* instr);
+	bool Execute();
+	bool Memory();
+	bool WriteBack();
+	bool NextInstruction();
+	std::pair<int, int> GetReturnRegisters();
+};
+
+// add other functions and objects here
